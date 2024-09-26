6573094431572269d41ccfd78d093d43e80100ad
erlm <erlm@kea.dk>
2024-09-26 11:44:42 +0200

    nu med billede

--- js/kommunetable.js
+++ js/kommunetable.js
@@ -1,12 +1,75 @@
-import {fetchAnyUrl} from "./modulejson.js";
+import {fetchAnyUrl, restDelete} from "./modulejson.js";
 console.log("er i kommunetable")
 
 const urlKommune = "http://localhost:8080/kommuner"
+const urlKommuneKode = "http://localhost:8080/kommune"
+
 const pbCreateKommuneTable = document.getElementById("pbGetKommuner")
 const tblKommuner = document.getElementById("tblKommuner")
 
 function createTable(kommune) {
-    console.log(kommune);
+    let cellCount = 0
+    let rowCount = tblKommuner.rows.length
+    let row = tblKommuner.insertRow(rowCount)
+    row.id = kommune.navn
+
+    let cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.kode
+    cell.style.width = "15%"
+
+    cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.navn
+    cell.style.width = "20%"
+
+    cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.href
+    cell.style.width = "15%"
+
+    if ((kommune.hrefPhoto.length) < 2) {
+        cell = row.insertCell(cellCount++)
+        cell.innerHTML = kommune.hrefPhoto
+        cell.style.width = "15%"
+    } else {
+        cell = row.insertCell(cellCount++)
+        let img = document.createElement("img")
+        img.setAttribute("src", kommune.hrefPhoto)
+        img.setAttribute("alt", "hej")
+        img.setAttribute("width", 150)
+        img.setAttribute("height", 150)
+        cell.appendChild(img)
+    }
+
+    cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.region.kode
+    cell.style.width = "15%"
+
+    cell = row.insertCell(cellCount++)
+    cell.innerHTML = kommune.region.navn
+    cell.style.width = "20%"
+
+    cell = row.insertCell(cellCount++)
+    const pbDelete = document.createElement("input");
+    pbDelete.type = "button";
+    pbDelete.setAttribute("value", "Slet kommune");
+    pbDelete.className = "btn1";
+    cell.appendChild(pbDelete);
+
+    pbDelete.onclick = function() {
+        document.getElementById(kommune.navn).remove();
+        deleteKommune(kommune);
+    }
+}
+
+async function deleteKommune(kommune) {
+    try {
+        const url = urlKommuneKode + "/" + kommune.kode
+        const resp = await restDelete(url)
+        const body = await resp.text();
+        alert(body)
+    } catch (error) {
+        alert(error.message);
+        console.log(error);
+    }
 }
 
 let kommuner = []
--- js/modulejson.js
+++ js/modulejson.js
@@ -4,5 +4,18 @@ function fetchAnyUrl(url) {
     return fetch(url).then(response => response.json())
 }
 
-export { fetchAnyUrl }
+async function restDelete(url) {
+    const fetchOptions = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: ""
+    }
+    const response = await fetch(url, fetchOptions)
+    return response
+}
+
+
+export { fetchAnyUrl, restDelete}
 
